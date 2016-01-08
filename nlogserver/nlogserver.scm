db975dd7db9cad351b4d544431f70a0b93ca1675
Kristian Lein-Mathisen <kristianlein@gmail.com>
2016-01-08 18:33:08 +0100

    cleanup: remove reser dependency
    because it's unofficial, embed reser's main ideas for now.
    
    this also fixes issue #1 too

--- nlogserver/nlogserver.scm
+++ nlogserver/nlogserver.scm
@@ -1,4 +1,41 @@
-(use files spiffy intarweb uri-common srfi-18 nrepl reser persistent-hash-map posix)
+(use files spiffy intarweb uri-common srfi-18 nrepl posix)
+
+
+;; ==================== reser ====================
+;; TODO: release reser egg and make don't embed it here
+(define (request-string!)
+  ;; TODO: what to do is we have more than 16MB? we can't just ignore it all.
+  (read-string (min (* 16 1024 1024) ;; <- max 16MB
+                    (or (header-value 'content-length (request-headers (current-request))) 0))
+               (request-port (current-request))))
+
+(define (reser-handler handler)
+
+  (define request `((body    . ,(request-string!))
+                    (uri     . ,(request-uri (current-request)))
+                    (headers . ,(request-headers (current-request)))
+                    (method  . ,(request-method (current-request)))))
+
+  (define resp (handler request))
+
+  (send-response body:    (or (alist-ref 'body resp) "")
+                 status:  (alist-ref 'status resp)
+                 code:    (alist-ref 'code resp)
+                 reason:  (alist-ref 'reason resp)
+                 headers: (or (alist-ref 'headers resp) '())))
+;; construct a response object
+(define (response #!key body status code reason headers)
+  `((body    . ,body)
+    (status  . ,status)
+    (code    . ,code)
+    (reason  . ,reason)
+    (headers . ,headers)))
+
+;; ====================       ====================
+
+(define (warn . args)
+  (with-output-to-port (current-error-port)
+    (lambda () (apply print args))))
 
 (define store-root (make-parameter #f))
 ;; (command-line-arguments '("./" "8080" "--ssl"))
@@ -23,12 +60,21 @@
     (newline port)
     (close-output-port port)))
 
+;; make sure x is serializable
+(define (serializable x)
+  (cond ((list? x) (map serializable x))
+        ((vector? x) (list->vector (map serializable (vector->list x))))
+        ((relative-ref? x) (uri->list x))
+        (else x)))
+;; (serializable `(1 #( ,(make-uri) ) 2))
+
 (define (app r)
   ;; try to make a serializeable scheme object from request (replace
   ;; records with lists etc)
-  (let ((reqobj `((ts . ,(current-seconds))
-                  (remote . ,(remote-address))
-                  ,@(map->alist (map-update-in r '(uri) uri->list)))))
+  (let ((reqobj (serializable
+                 `((ts . ,(current-seconds))
+                   (remote . ,(remote-address))
+                   ,@r))))
 
     ;; write directly to file:
     (save reqobj (req->filename r))
@@ -37,8 +83,6 @@
       (lambda () (pp reqobj) (flush-output)))
 
     (response body: "{\"status\" : \"ok\"}\n")))
-
-(define handler (wrap-errors (wrap-log app)))
 
 (define server #f)
 (cond ((member "--ssl" (cla))
@@ -55,7 +99,7 @@
 (define nrepl-thread  (thread-start! (lambda () (nrepl (+ 1 (server-port))))))
 (define server-thread
   (thread-start! (lambda ()
-                   (vhost-map `((".*" . ,(lambda (c) (reser-handler handler)))))
+                   (vhost-map `((".*" . ,(lambda (c) (reser-handler (lambda (r) (app r)))))))
                    (server))))
 
 (thread-join! server-thread)

49abdb6d02f7fca36cfe0e32ebfac3fa7f6b2cb8
Kristian Lein-Mathisen <kristianlein@gmail.com>
2016-01-12 19:23:16 +0100

    nlog cla: add optional -c option
    in case you don't want to use nlogd

--- nlog.scm
+++ nlog.scm
@@ -1,7 +1,12 @@
 (include "libnlog.scm")
 
+(define send-log send-log-nanomsg)
+
 (if (member "-d" (command-line-arguments))
     (debug? #t))
+
+(if (member "-c" (command-line-arguments))
+    (set! send-log send-log-http))
 
 (define (texts)
   (filter (lambda (x) (not (string-prefix? "-" x)))
@@ -11,5 +16,5 @@
 (define msgs (map create-message (texts)))
 
 (if (pair? msgs)
-    (for-each send-log-nanomsg msgs)
-    (port-for-each (lambda (line) (send-log-http (create-message line))) read-line))
+    (for-each send-log msgs)
+    (port-for-each (lambda (line) (send-log (create-message line))) read-line))

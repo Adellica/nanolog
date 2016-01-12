8e8aeb2e1528fec658406cd79dc1b32d18890caf
Kristian Lein-Mathisen <kristianlein@gmail.com>
2016-01-12 17:55:18 +0100

    cla: don't send "options" as messages
    nlog -d
    will previously send "-d" as a message.

--- nlog.scm
+++ nlog.scm
@@ -3,7 +3,11 @@
 (if (member "-d" (command-line-arguments))
     (debug? #t))
 
-(define msgs (map create-message (command-line-arguments)))
+(define (texts)
+  (filter (lambda (x) (not (string-prefix? "-" x)))
+          (command-line-arguments)))
+
+(define msgs (map create-message (texts)))
 
 (if (pair? msgs)
     (for-each send-log-http msgs)
